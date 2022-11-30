07da0927fb475c848d634f6f78688f4fb6c3bb73
Ronildo Sousa <sousaronildo400@gmail.com>
2022-11-30 20:17:41 -0300

    coin card

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import CoinPriceCard from './components/CoinPriceCard.vue'
+
+const selectedCoin =  
+    {
+        "name": "Bitcoin",
+        "symbol": "BTC",
+        "date": "30-11-2022 14:08:15",
+        "price": 16925.06
+    }
+
+// onMounted(() => axios.get("http://localhost/api/coins").then(response => {
+//     coins.value = response.data
+// }))
+</script>
+
+<template>
+    <CoinPriceCard :coin="selectedCoin" />
+</template>
