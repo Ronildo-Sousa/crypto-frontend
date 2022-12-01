3c53fa5fb0941501b42b34854156e47ff86d5a9e
Ronildo Sousa <sousaronildo400@gmail.com>
2022-11-30 22:27:59 -0300

    initializing recent price feature

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,88 @@
 <script setup>
+import { ref, watch, onBeforeMount } from 'vue'
+import axios from 'axios'
 import CoinPriceCard from './components/CoinPriceCard.vue'
 
-const selectedCoin =  
-    {
-        "name": "Bitcoin",
-        "symbol": "BTC",
-        "date": "30-11-2022 14:08:15",
-        "price": 16925.06
-    }
+const BASE_URL = "http://localhost/api/"
 
-// onMounted(() => axios.get("http://localhost/api/coins").then(response => {
-//     coins.value = response.data
-// }))
+const coins = ref([
+    {
+        "id": 1,
+        "symbol": "BTC",
+        "identifier": "bitcoin"
+    },
+    {
+        "id": 2,
+        "symbol": "ATOM",
+        "identifier": "cosmos"
+    },
+])
+
+const getRecentPrice = (coin) => {
+    axios.get(BASE_URL + "price/" + coin).then((response) => {
+        coinInfo.value  = response.data.recent_price
+    })
+}
+
+const getHistoryPrice = (coin) => {
+    console.log(coin)
+}
+
+let coinInfo = ref(null)
+
+const openDefaultMenu = ref(true)
+const changeMenu = (value) => {
+    openDefaultMenu.value = value
+}
+
+const selectedCoin = ref(coins.value[0].name)
+
+watch(selectedCoin, () => {
+    if (openDefaultMenu) {
+        getRecentPrice(selectedCoin.value)
+    }
+})
+
+onBeforeMount(() => {
+    getRecentPrice("bitcoin")
+})
+
+// const selectedCoin =
+// {
+//     "name": "Bitcoin",
+//     "symbol": "BTC",
+//     "date": "30-11-2022 14:08:15",
+//     "price": 16925.06
+// }
 </script>
 
 <template>
-    <CoinPriceCard :coin="selectedCoin" />
+    <main class="text-center">
+        <header class="flex items-center justify-center h-16 bg-green-400">
+            <h1 class="text-2xl font-bold">Crypto App</h1>
+        </header>
+        <div class="mt-3 mb-3">
+            <button @click="changeMenu(true)" :class="{'underline': openDefaultMenu}" class="text-lg">
+                Recent price
+            </button>
+            <span class="mx-8 text-gray-300">|</span>
+            <button @click="changeMenu(false)" :class="{ 'underline': !openDefaultMenu }" class="text-lg">
+                History price
+            </button>
+        </div>
+        <div class="flex justify-center">
+            <div v-if="openDefaultMenu" class="flex items-center justify-around w-full max-w-md mt-8 mb-8 md:w-3/4">
+                <span>Get recent price of: </span>
+                <select v-model="selectedCoin" id="coins" class="w-2/4 p-1 shadow-lg rounde-md">
+                    <option v-for="coin in coins" :value="coin.identifier" :key="coin.id">{{ coin.symbol }}</option>
+                </select>
+            </div>
+            <div v-else>
+                menu 2
+            </div>
+        </div>
+        <div class="flex items-center justify-center">
+            <CoinPriceCard class="w-full" :coin="coinInfo" />
+        </div>
+    </main>
 </template>
--- src/components/CoinPriceCard.vue
+++ src/components/CoinPriceCard.vue
@@ -1,18 +1,21 @@
 <script setup>
-defineProps({
+import { computed } from 'vue'
+
+const props = defineProps({
     coin: Object
 })
+
 </script>
 
 <template>
-    <section class="flex flex-col items-center max-w-sm p-3 shadow-lg">
+    <section class="flex flex-col items-center max-w-sm p-3 border border-gray-300 rounded-md shadow-md">
         <div class="flex justify-around w-full mb-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="w-6 h-6 text-green-500">
                 <path stroke-linecap="round" stroke-linejoin="round"
                     d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
-            <p class="mr-3 text-2xl font-semibold">{{ coin.name }}</p>
+            <h1 class="mr-3 text-2xl font-semibold">{{ coin.name }}</h1>
             <span>{{ coin.symbol }}</span>
         </div>
         <p class="mb-3 text-3xl font-bold">$ {{ coin.price }}</p>
